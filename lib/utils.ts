eed1d418abf59774b4629077405ddabc7c9db79e
vae <18137693952@163.com>
2025-03-17 17:20:35 +0800

    feat: ai / 创建对话时本地生成对话 id

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx'
 import { NextResponse } from 'next/server'
 import { twMerge } from 'tailwind-merge'
+import { v4 as uuidv4 } from 'uuid'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -22,4 +23,9 @@ export async function getFileHash(file: File) {
   const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
   return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+}
+
+// 生成uuid 支持 withHyphens 参数为 false 时不带 -
+export function generateUUID(withHyphens = true) {
+  return withHyphens ? uuidv4() : uuidv4().replaceAll('-', '')
 }
