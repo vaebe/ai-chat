04c0b393d22f475757b1e2df3d46c4dcd812b85b
vae <18137693952@163.com>
2025-03-17 14:51:29 +0800

    style(ui): 优化页面样式和布局细节

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ export default function Home() {
   return (
     <div className="dark min-h-screen bg-black text-white">
       <header className="flex items-center justify-between px-6 py-2">
-        <div className="text-2xl font-bold">AI Chat</div>
+        <div className="text-3xl font-bold">AI Chat</div>
 
         <Link href="/login">
-          <Button>登录</Button>
+          <Button className="cursor-pointer">登录</Button>
         </Link>
       </header>
 
       <div className="w-9/12 mx-auto my-10">
-        <img src="/img/chat-dark.png" alt="" className="w-full" />
+        <picture>
+          <img src="/img/chat-dark.png" alt="" className="w-full" />
+        </picture>
       </div>
 
       <p className="text-center text-lg">一个使用 next 开发 AI 对话页面完善中...</p>
