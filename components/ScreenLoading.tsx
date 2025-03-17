023ae84fbcd2fb3ca0e01dc7ef06523db423f344
vae <18137693952@163.com>
2025-03-17 16:03:54 +0800

    style: 样式优化

--- components/ScreenLoading.tsx
+++ components/ScreenLoading.tsx
@@ -11,7 +11,7 @@ export const FullScreenLoading: FC<Props> = ({ isLoading, message = 'Loading...'
   if (!isLoading) return null
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-[99999] flex items-center justify-center bg-black/40 backdrop-blur-sm">
+    <div className="fixed inset-0 z-[99999] flex items-center justify-center bg-black/40 backdrop-blur-xs">
       <div className="flex flex-col items-center">
         <div className="w-16 h-16 border-t-4 border-white border-solid rounded-full animate-spin"></div>
         <p className="mt-4 text-lg font-semibold text-white">{message}</p>
