762eb35f8c2530e6f1fb3a8c51307383c134df70
vae <18137693952@163.com>
2025-04-07 10:52:49 +0800

    chore: build 配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  serverExternalPackages: ['@modelcontextprotocol/sdk']
 }
 
 export default nextConfig
