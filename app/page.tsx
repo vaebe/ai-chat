0b702085cdcb8e67030a97b58e64a900e4562117
vae <18137693952@163.com>
2025-08-28 18:11:51 +0800

    fix: 未使用的引入

--- app/page.tsx
+++ app/page.tsx
@@ -6,43 +6,57 @@ import { Button } from '@/components/ui/button'
 import { KeySquare, Key } from 'lucide-react'
 import { ThemeSwitch } from '@/components/theme-switch'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+function HomeHeader() {
   const { theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
 
-  const isDark = theme === 'dark'
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
+    <header className="flex items-center justify-between px-6 py-2">
+      <div className="flex items-center">
+        <Image
+          src={theme === 'dark' ? '/logo/light.svg' : '/logo/dark.svg'}
+          alt="logo"
+          width={38}
+          height={38}
+        />
+        <div className="text-3xl font-bold">Chat</div>
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <ThemeSwitch></ThemeSwitch>
+
+        <Link href="/sign-up">
+          <Button className="cursor-pointer" variant="outline" size="sm">
+            <Key />
+            注册
+          </Button>
+        </Link>
+
+        <Link href="/sign-in">
+          <Button className="cursor-pointer" variant="outline" size="sm">
+            <KeySquare />
+            登录
+          </Button>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default function Home() {
+  return (
     <div className="min-h-screen dark:bg-black dark:text-white">
-      <header className="flex items-center justify-between px-6 py-2">
-        <div className="flex items-center">
-          <img
-            src={theme === 'dark' ? '/logo/light.svg' : '/logo/dark.svg'}
-            alt="logo"
-            width={38}
-            height={38}
-          />
-          <div className="text-3xl font-bold">Chat</div>
-        </div>
-
-        <div className="flex items-center space-x-2">
-          <ThemeSwitch></ThemeSwitch>
-
-          <Link href="/sign-up">
-            <Button className="cursor-pointer" variant="outline" size="sm">
-              <Key />
-              注册
-            </Button>
-          </Link>
-
-          <Link href="/sign-in">
-            <Button className="cursor-pointer" variant="outline" size="sm">
-              <KeySquare />
-              登录
-            </Button>
-          </Link>
-        </div>
-      </header>
+      <HomeHeader></HomeHeader>
 
       <div className="w-11/12 md:w-9/12 mx-auto my-10">
         <Image
