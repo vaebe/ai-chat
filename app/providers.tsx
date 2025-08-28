1d8f491f3731389f29bd6e44e61c5651ed1e44fe
vae <18137693952@163.com>
2025-08-28 17:04:46 +0800

    feat(theme): add theme switching functionality with dark mode support

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,30 @@
 'use client'
 
 import '@/lib/date'
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
 import { Toaster } from '@/components/ui/sonner'
 import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+
+function ClerkWithThemeProvider({ children }: { children: React.ReactNode }) {
+  const { resolvedTheme } = useTheme()
+
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: resolvedTheme === 'dark' ? dark : undefined
+      }}
+    >
+      {children}
+      <Toaster position="top-center" />
+    </ClerkProvider>
+  )
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-        <Toaster position="top-center" />
-      </ThemeProvider>
-    </ClerkProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ClerkWithThemeProvider>{children}</ClerkWithThemeProvider>
+    </ThemeProvider>
   )
 }
